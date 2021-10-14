f7da86b8b242d4b85bfcdc80977198c24dbc6fc9
tokikou <tokikou1588@gmail.com>
2021-10-14 22:32:33 +0900

    2

---
+++ SlarkInc/SlarkInc/Controllers/FirstController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+namespace SlarkInc.Controllers
+{
+    public class FirstController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Number = 8;
+            ViewBag.Message = "This is index Page";
+            ViewBag.Slarks = new List<string> { "Slark1", "Slark2", "Slark3" };
+            return View();
+        }
+	}
+}
---
+++ SlarkInc/SlarkInc/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+namespace SlarkInc.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
